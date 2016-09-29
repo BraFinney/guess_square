35736ad5886995c46d04217dc4b205384b9eb8e2
Charles Opoku-Agyemang <micnkru@gmail.com>
2016-09-29 08:46:44 +0000

    Update guess_sqrt.rb

--- guess_sqrt.rb
+++ guess_sqrt.rb
@@ -1,22 +1,18 @@
-def square(b)
-	b*b
-end	
-
-def guess_sqrt (a)
-	if a % 2 ==0
-	c = a/2
-	until ((square(c) - a.to_i).abs) <= 0.0000000001 do
-		c -= 1
-	end 
-	puts c 
+def sqr(x); x**2; end
+def guess_sqrt (a,b)
+	if a % 2 == 0
+		c = a / 2
+		until (sqr(c) - a).abs <= b
+			c -= 1		
+		end
 	else
 		d = a - 1
-		c = d/2
-		until ((square(c) - a.to_i).abs) <= 0.0000000001 do
-		c -= 1
-	end 
-	puts c 
-	end
-end	
+		c = d / 2
+		until (sqr(c) - a).abs <= b
+			c -= 1		
+		end	
+	end	
+	puts c
+end 
 
-guess_sqrt(1000000)
+guess_sqrt(1000000,0.0000000000001)
